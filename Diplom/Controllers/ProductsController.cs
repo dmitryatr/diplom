d7f743490cdabffc93986fd872b656c7ce43bfea
Dmitry Atrashkov <dimonatrashkov@gmail.com>
2018-05-23 16:45:34 +0300

    Implemented addition of the product to database

--- Diplom/Controllers/ProductsController.cs
+++ Diplom/Controllers/ProductsController.cs
@@ -1,16 +1,45 @@
-﻿using System;
+﻿using AutoMapper;
+using Diplom.Models;
+using Domain.Entities;
+using Domain.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Domain.Abstract;
 
 namespace Diplom.Controllers
 {
     public class ProductsController : Controller
     {
+        IProductRepository repository;
+
+        public ProductsController(IProductRepository r)
+        {
+            repository = r;
+        }
+
         public ActionResult AddProduct()
         {
             return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(AddProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Mapper.Initialize(map => map.CreateMap<AddProductViewModel, Product>());
+                Product product = Mapper.Map<AddProductViewModel, Product>(model);
+                product.DateUpdate = DateTime.Now;
+                product.UserID = (from one_user in repository.Users
+                                  where one_user.Name == User.Identity.Name
+                                  select one_user.ID).FirstOrDefault(); ;
+                product.CategoryID = 1;
+                repository.AddProduct(product);
+            }
+            return PartialView(model);
         }
     }
 }
--- Diplom/Models/AddProductViewModel.cs
+++ Diplom/Models/AddProductViewModel.cs
@@ -15,25 +15,26 @@ namespace Diplom.Models
         [Display(Name = "Название")]
         public string Name { get; set; }
 
+        [Required]
         [Display(Name = "Описание")]
         public string Description { get; set; }
 
         [Display(Name = "Состояние")]
         public string State { get; set; }
 
+        [Required]
         [Display(Name = "Оплата")]
         public string Payment { get; set; }
 
         [Display(Name = "Доставка")]
         public string Delivery { get; set; }
 
-        [Display(Name = "Дата обновления")]
-        public DateTime DateUpdate { get; set; }
-
+        [Required]
+        [Display(Name = "Город")]
         public string City { get; set; }
 
-        [Display(Name = "Цена")]
         [Required]
+        [Display(Name = "Цена")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     }
